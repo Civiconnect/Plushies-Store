3e4afe1454b77e7fb174c215ebc8469d7d3f5f57
Stany Chabikuli <schabikuli@civiconnect.ca>
2023-01-18 16:10:19 -0500

    -

--- scripts/render-products.js
+++ scripts/render-products.js
@@ -1,6 +1,6 @@
 console.log("Hello, I'm a script!")
 // get the JSON data from http://localhost:5500/plushies.json
-const response = await fetch("/plushies.json");
+const response = await fetch("/Plushies-Store/plushies.json");
 // parse out the json into a variable - "plushies"
 const plushies = await response.json();
 
